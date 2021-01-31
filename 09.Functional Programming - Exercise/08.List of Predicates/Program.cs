3d7f03996d47901d12b6f21b7548e2470f8604ea
Ivan Hristoslavov <hristoslavov.ivanov@gmail.com>
2021-01-31 12:28:48 +0200

    Add exercise

---
+++ 09.Functional Programming - Exercise/08.List of Predicates/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08.List_of_Predicates
+{
+    class Programa
+    {
+        static void Main(string[] args)
+        {
+            Func<List<int>, int, List<int>> func = (list, n) =>
+              {
+                  List<int> devidetNumbers = new List<int>();
+
+                  for (int i = 1; i <= n; i++)
+                  {
+                      if (list.All(x => i % x == 0))
+                      {
+                          devidetNumbers.Add(i);
+                      }
+                  }
+                  return devidetNumbers;
+              };
+
+            int n = int.Parse(Console.ReadLine());
+            List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            List<int> outputNumbers = func(numbers, n);
+
+            Action<List<int>> print = numbers => Console.WriteLine(string.Join(" ", numbers));
+            print(outputNumbers);
+
+        }
+    }
+}
