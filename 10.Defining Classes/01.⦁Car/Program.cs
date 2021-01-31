5cdbe9540d6287b95c347fa4c69cffbde486dbe8
Ivan Hristoslavov <hristoslavov.ivanov@gmail.com>
2021-01-31 14:48:56 +0200

    Classes

---
+++ 10.Defining Classes/01.⦁Car/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+       public static void Main(string[] args)
+        {
+
+            Car car = new Car();
+
+            car.Make = "VW";
+            car.Model = "MK3";
+            car.Year = 1992;
+
+            Console.WriteLine($"Make: {car.Make}\nMolde: {car.Model}\nYear: {car.Year}");
+
+
+
+
+        }
+    }
+}
---
+++ 10.Defining Classes/04.Car Engine and Tires/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            Engine v8 = new Engine(306, 5000);
+            Tire[] tires = new Tire[]
+            {
+                new Tire(2019,2.5),
+            new Tire(2018, 2.4),
+            new Tire(2017, 2.2),
+            new Tire(2020, 2.1),
+            };
+
+            Car mercedes = new Car("Mercedes", "CLS", 2005, -20,5000, v8, tires);
+            Console.WriteLine(mercedes.Engine.HorsePower);
+
+            foreach (var tire in mercedes.Tires)
+            {
+
+                Console.WriteLine($"{tire.Year} - {tire.Pressure}");
+
+            }
+
+
+        }
+    }
+}
