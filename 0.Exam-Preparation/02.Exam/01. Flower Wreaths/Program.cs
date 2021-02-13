c0af8063f5dadcd9590f9e8ec35e7e803e68e89c
IvHrIvanov <hristoslavov.ivanov@gmail.com>
2021-02-13 21:20:22 +0200

    Add exercises and change old

---
+++ 0.Exam-Preparation/02.Exam/01. Flower Wreaths/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Flower_Wreaths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] liliesAdd = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int[] rosesAdd = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            Stack<int> lilies = new Stack<int>(liliesAdd);
+            Queue<int> roses = new Queue<int>(rosesAdd);
+
+            int wreath = 0;
+            int n = 0;
+            while (lilies.Count != 0 && roses.Count != 0)
+            {
+                int roseValue = roses.Peek();
+                int liliesValue = lilies.Peek();
+
+                int sum = roseValue + liliesValue;
+
+                if (sum == 15)
+                {
+                    roses.Dequeue();
+                    lilies.Pop();
+                    wreath++;
+                }
+                else if (sum > 15)
+                {
+                    int decreaseValue = lilies.Pop();
+                    lilies.Push(decreaseValue - 2);
+                }
+                else
+                {
+                    n += sum;
+                    roses.Dequeue();
+                    lilies.Pop();
+                }
+
+            }
+
+            if (wreath >= 5)
+            {
+                Console.WriteLine($"You made it, you are going to the competition with {wreath} wreaths!" );
+            }
+            else
+            {
+                int needet = n/15+wreath;
+                Console.WriteLine($"You didn't make it, you need {5-needet} wreaths more!");
+            }
+
+        }
+    }
+}
---
+++ 0.Exam-Preparation/02.Exam/02. Bee/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Text;
+
+namespace _02._Bee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int size = int.Parse(Console.ReadLine());
+            char[,] mattrix = new char[size, size];
+            int rowBee = 0;
+            int colBee = 0;
+            int bonusRow = 0;
+            int bonusCol = 0;
+            for (int i = 0; i < size; i++)
+            {
+                string input = Console.ReadLine();
+                for (int j = 0; j < size; j++)
+                {
+                    if (input[j] == 'B')
+                    {
+                        rowBee = i;
+                        colBee = j;
+                    }
+                    if (input[j] == 'O')
+                    {
+                        bonusRow = i;
+                        bonusCol = j;
+                    }
+                    mattrix[i, j] = input[j];
+
+                }
+            }
+            int polinationed = 0;
+            string command = Console.ReadLine();
+            bool flag = true;
+            bool lost = false;
+            while (command != "End")
+            {
+                mattrix[rowBee, colBee] = '.';
+                if (command == "up")
+                {
+                    rowBee--;
+                }
+                else if (command == "down")
+                {
+                    rowBee++;
+                }
+                else if (command == "right")
+                {
+                    colBee++;
+                }
+                else if (command == "left")
+                {
+                    colBee--;
+                }
+                flag = isValid(rowBee, colBee, size);
+                if (flag)
+                {
+                    char symbol = mattrix[rowBee, colBee];
+
+                    if (symbol == 'f')
+                    {
+                        polinationed++;
+                    }
+                    else if (symbol == 'O')
+                    {
+                        mattrix[rowBee, colBee] = '.';
+                        if(command=="up")
+                        {
+                            rowBee--;
+                        }
+                        else if( command=="left")
+                        {
+                            colBee--;
+                        }
+                        else if (command == "right")
+                        {
+                            colBee++;
+
+                        }
+                        else if (command == "down")
+                        {
+                            rowBee++;
+
+                        }
+                        symbol = mattrix[rowBee, colBee];
+                        if (symbol == 'f')
+                        {
+
+                            polinationed++;
+                        }
+                    }
+
+                    mattrix[rowBee, colBee] = 'B';
+                }
+                else
+                {
+                    lost = true;
+                    break;
+                }
+                command = Console.ReadLine();
+            }
+            StringBuilder sb = new StringBuilder();
+
+            if (lost)
+            {
+                sb.AppendLine("The bee got lost!");
+            }
+            if (5 <= polinationed)
+            {
+                sb.AppendLine($"Great job, the bee managed to pollinate {polinationed} flowers!");
+            }
+            else
+            {
+                int needed = 5 - polinationed;
+                sb.AppendLine($"The bee couldn't pollinate the flowers, she needed {needed} flowers more");
+            }
+            Console.Write(sb);
+            for (int row = 0; row < size; row++)
+            {
+                for (int col = 0; col < size; col++)
+                {
+                    Console.Write(mattrix[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static bool isValid(int rowBee, int colBee, int size)
+        {
+            return rowBee < size && colBee < size && colBee >= 0 && rowBee >= 0;
+        }
+    }
+}
