31c5c6f2916b45a373b158ea46ac65ccd7af0b2e
IvHrIvanov <hristoslavov.ivanov@gmail.com>
2021-02-14 17:29:22 +0200

    Add task

---
+++ 0.Exam-Preparation/02.Exam/03.VetClinic/Clinic.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VetClinic
+{
+    public class Clinic
+    {
+        private List<Pet> pet;
+        public Clinic(int capacity)
+        {
+            Capacity = capacity;
+            Pet = pet;
+        }
+        public int Count { get; set; }
+        public int Capacity { get; set; }
+
+        public List<Pet> Pet { get; set; }
+
+        public void Add(Pet pet)
+        {
+            if (Count < Capacity)
+            {
+                if (this.Pet == null)
+                {
+                    this.Pet = new List<Pet>();
+                }
+                Count++;
+                this.Pet.Add(pet);
+            }
+        }
+        public bool Remove(string name)
+        {
+            Pet current = Pet.FirstOrDefault(x => x.Name == name);
+            if (Pet.Contains(current))
+            {
+                Count--;
+                Pet.Remove(current);
+                return true;
+            }
+            return false;
+
+        }
+
+        public Pet GetOldestPet()
+        {
+
+            Pet oldestPet = null;
+            foreach (Pet item in this.Pet)
+            {
+                if (oldestPet == null || item.Age > oldestPet.Age)
+                {
+                    oldestPet = item;
+                }
+
+            }
+            return oldestPet;
+        }
+
+        public string GetStatistics()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("The clinic has the following patients:");
+            foreach (Pet item in this.Pet)
+            {
+                sb.AppendLine($"Pet {item.Name} with owner: {item.Owner}");
+            }
+            
+            return sb.ToString().TrimEnd();
+        }
+
+        public Pet GetPet(string name, string owner)
+        {
+            Pet current = null;
+            bool flag = false;
+            foreach (Pet item in Pet)
+            {
+                if(item.Name==name && item.Owner==owner)
+                {
+                    flag = true;
+                    current = item;
+                }
+            }
+            if (flag)
+            {
+                return current;
+            }
+            return null;
+        }
+    }
+
+
+}
+
---
+++ 0.Exam-Preparation/02.Exam/03.VetClinic/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace VetClinic
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+
+        {
+            // Initialize the repository
+            Clinic clinic = new Clinic(20);
+
+            // Initialize entity
+            Pet dog = new Pet("Ellias", 5, "Tim");
+
+            // Print Pet
+            Console.WriteLine(dog); // Ellias 5 (Tim)
+
+            // Add Pet
+            clinic.Add(dog);
+
+            // Remove Pet
+            Console.WriteLine(clinic.Remove("Ellias")); // True
+            Console.WriteLine(clinic.Remove("Pufa")); // False
+        
+            Pet cat = new Pet("Bella", 2, "Mia");
+            Pet bunny = new Pet("Zak", 4, "Jon");
+
+            clinic.Add(cat);
+            clinic.Add(bunny);
+          
+            // Get Oldest Pet
+            Pet oldestPet = clinic.GetOldestPet();
+            Console.WriteLine(oldestPet); // Zak 4 (Jon)
+
+            Pet pet = clinic.GetPet("Bella", "Mia");
+            Console.WriteLine(pet); // Bella 2 (Mia)
+            Console.WriteLine(clinic.Count); // 2
+
+            Console.WriteLine(clinic.GetStatistics());
+
+        }
+    }
+}
