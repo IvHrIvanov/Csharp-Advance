673eb25e2fb7089d1a31793619769da000b49de8
IvHrIvanov <hristoslavov.ivanov@gmail.com>
2021-02-17 17:22:04 +0200

    Add & update

---
+++ 0.Exam-Preparation/03.Exam/CAdvancedExam-25October2020/03.Classroom/Classroom.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassroomProject
+{
+    public class Classroom
+    {
+        private List<Student> data;
+
+        public Classroom(int capacity)
+        {
+            Capacity = capacity;
+            data = new List<Student>();
+        }
+
+        public int Capacity { get; set; }
+        public int Count { get { return data.Count; } }
+
+        public string RegisterStudent(Student student)
+        {
+            if (data.Count < Capacity)
+            {
+                data.Add(student);
+                return $"Added student {student.FirstName} {student.LastName}";
+            }
+            return "No seats in the classroom";
+        }
+        public string DismissStudent(string firstName, string lastName)
+        {
+            Student current = data.FirstOrDefault(x => x.FirstName == firstName && x.LastName == lastName);
+            if (data.Contains(current))
+            {
+                data.Remove(current);
+                return $"Dismissed student {firstName} {lastName}";
+            }
+            return "Student not found";
+        }
+        public string GetSubjectInfo(string subject)
+        {
+            List<Student> students = new List<Student>();
+            foreach (Student item in data)
+            {
+                if (item.Subject == subject)
+                {
+                    students.Add(item);
+                }
+            }
+            StringBuilder sb = new StringBuilder();
+            if (students.Any(x => x.Subject == subject))
+            {
+                sb.AppendLine($"Subject: {subject}");
+                sb.AppendLine("Students:");
+                foreach (Student item in students)
+                {
+                    sb.AppendLine($"{item.FirstName} {item.LastName}");
+                }
+            }
+            else
+            {
+                sb.AppendLine("No students enrolled for the subject");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+        public int GetStudentsCount()
+        {
+            return data.Count;
+        }
+        public Student GetStudent(string firstName, string lastName)
+        {
+           
+            return data.FirstOrDefault(x => x.FirstName == firstName && x.LastName == lastName);
+        }
+    }
+}
---
+++ 0.Exam-Preparation/04.Exam/CAdvanced Exam - 28 June 2020/01.Bombs/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _01.Bombs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Dictionary<string, int> bombs = new Dictionary<string, int>();
+            int[] inputEfect = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int[] inputCasing = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            Queue<int> efectBomb = new Queue<int>(inputEfect);
+            Stack<int> casingBomb = new Stack<int>(inputCasing);
+
+            int daturaBombs = 0;
+            int cherryBombs = 0;
+            int smokeDecoyBombs = 0;
+            bool flag = false;
+            while (efectBomb.Count != 0 && casingBomb.Count != 0)
+            {
+                int efect = efectBomb.Peek();
+                int casing = casingBomb.Peek();
+                int sum = efect + casing;
+
+                if (sum == 40)
+                {
+                    daturaBombs++;
+                    RemoveBomb(casingBomb, efectBomb);
+                }
+                else if (sum == 60)
+                {
+                    cherryBombs++;
+                    RemoveBomb(casingBomb, efectBomb);
+                }
+                else if (sum == 120)
+                {
+                    smokeDecoyBombs++;
+                    RemoveBomb(casingBomb, efectBomb);
+                }
+                else
+                {
+                    int decrese = casingBomb.Pop() - 5;
+                    casingBomb.Push(decrese);
+                }
+                if (daturaBombs == 3 && cherryBombs == 3 && smokeDecoyBombs == 3)
+                {
+                    flag = true;
+                    break;
+                }
+            }
+            StringBuilder sb = new StringBuilder();
+            if (flag)
+            {
+                sb.AppendLine("Bene! You have successfully filled the bomb pouch!");
+            }
+            else
+            {
+                sb.AppendLine("You don't have enough materials to fill the bomb pouch.");
+            }
+            if (efectBomb.Count == 0)
+            {
+                sb.AppendLine("Bomb Effects: empty");
+            }
+            else
+            {
+                sb.AppendLine($"Bomb Effects: {string.Join(", ",efectBomb)}" );
+            }
+            if (casingBomb.Count == 0)
+            {
+                sb.AppendLine("Bomb Casings: empty");
+            }
+            else
+            {
+                sb.AppendLine($"Bomb Effects: {string.Join(", ", casingBomb)}");
+            }
+            sb.AppendLine($"Cherry Bombs: {cherryBombs}");
+            sb.AppendLine($"Datura  Bombs: {daturaBombs}");
+            sb.AppendLine($"Smoke Decoy Bombs: {smokeDecoyBombs}");
+
+            Console.WriteLine(sb);
+        }
+
+        private static void RemoveBomb(Stack<int> casingBomb, Queue<int> efectBomb)
+        {
+            casingBomb.Pop();
+            efectBomb.Dequeue();
+        }
+    }
+}
