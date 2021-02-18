6fda5874cfaa30c67a6ac93ad1d8edd9890ea643
IvHrIvanov <hristoslavov.ivanov@gmail.com>
2021-02-18 19:56:14 +0200

    Add task

---
+++ 0.Exam-Preparation/04.Exam/03.Parking/Parking.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Parking
+{
+    public class Parking
+    {
+        private List<Car> data;
+
+        public Parking(string type, int capacity)
+        {
+            Type = type;
+            Capacity = capacity;
+            data = new List<Car>();
+        }
+        public int Count { get { return data.Count; } }
+        public string Type { get; set; }
+        public int Capacity { get; set; }
+
+        public void Add (Car car)
+        {
+            if(data.Count<Capacity)
+            {
+                data.Add(car);
+            }
+        }
+        public bool Remove(string manufacturer, string model)
+        {
+            if (data.Any(x => x.Manufacturer == manufacturer && x.Model == model))
+            {
+                data.Remove(data.FirstOrDefault(x => x.Manufacturer==manufacturer && x.Model == model));
+                return true;
+            }
+            return false;
+
+        }
+        public Car GetLatestCar()
+        {
+           
+            return data.OrderByDescending(x => x.Year).FirstOrDefault();        
+        }
+        public Car GetCar(string manufacturer, string model)
+        {
+            return data.FirstOrDefault(x => x.Manufacturer == manufacturer && x.Model == model);
+        }
+      
+        public string GetStatistics()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"The cars are parked in {Type}:");
+            foreach (Car item in data)
+            {
+                sb.AppendLine(item.ToString());
+            }
+            return sb.ToString().TrimEnd();      
+        }
+    }
+}
---
+++ 0.Exam-Preparation/04.Exam/04.ClubParty/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.ClubParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<int>> hall = new Dictionary<string, List<int>>();
+            List<string> rooms = new List<string>();
+            int capacity = int.Parse(Console.ReadLine());
+            string[] input = Console.ReadLine().Split(" ");
+            Stack<string> currentChar = new Stack<string>(input);
+
+            while (currentChar.Count != 0)
+            {
+                char[] chars = currentChar.Peek().ToCharArray();
+
+                if (Char.IsLetter(chars[0]))
+                {
+                    string inputHall = currentChar.Pop();
+
+                    if (hall.Count == 0)
+                    {
+                        hall.Add(inputHall, new List<int>());
+                    }
+
+                    rooms.Add(inputHall);
+                }
+                else
+                {
+                    int addPeople = int.Parse(currentChar.Pop());
+                    if (hall.Count != 0)
+                    {
+                        string currentHal = rooms[0];
+                        int sum = hall[currentHal].Sum() + addPeople;
+
+                        if (sum <= capacity)
+                        {
+                            hall[currentHal].Add(addPeople);
+                        }
+                        else
+                        {
+                            if (hall[currentHal].Sum() <= 60)
+                            {
+                                foreach (var item in hall)
+                                {
+                                    Console.WriteLine($"{item.Key} -> {string.Join(", ", item.Value)}");
+                                }
+                                hall.Remove(currentHal);
+                            }
+
+                            rooms.RemoveAt(0);
+
+                            if (rooms.Count != 0)
+                            {
+                                string nextRoom = rooms[0];
+                                hall.Add(nextRoom, new List<int>());
+                                hall[nextRoom].Add(addPeople);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
